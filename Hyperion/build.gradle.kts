e64313513199e11057a68ffd364a7d9f20be4e59
Gilbert O. <giloe.dev@gmail.com>
2024-02-24 23:29:53 -0700

    Remove file reading and parse with rust and replaced with methods

--- Hyperion/build.gradle.kts
+++ Hyperion/build.gradle.kts
@@ -1,13 +1,13 @@
-import com.android.build.gradle.tasks.MergeSourceSetFolders
-import com.nishtahir.CargoBuildTask
-import com.nishtahir.CargoExtension
+//import com.android.build.gradle.tasks.MergeSourceSetFolders
+//import com.nishtahir.CargoBuildTask
+//import com.nishtahir.CargoExtension
 
 val ftcVersion = "9.1.0"
 
 plugins {
 	id("com.android.library")
 	id("org.jetbrains.kotlin.android")
-	id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3"
+//	id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3"
 }
 
 android {
@@ -33,15 +33,15 @@ android {
 		}
 	}
 
-	extensions.configure(CargoExtension::class.java) {
-		module = "./src/main/rust"
-		libname = "hyperion"
-		targets = listOf("arm", "arm64")
-	}
-
-	tasks.preBuild.configure {
-		dependsOn.add(tasks.withType(CargoBuildTask::class.java))
-	}
+//	extensions.configure(CargoExtension::class.java) {
+//		module = "./src/main/rust"
+//		libname = "hyperion"
+//		targets = listOf("arm", "arm64")
+//	}
+//
+//	tasks.preBuild.configure {
+//		dependsOn.add(tasks.withType(CargoBuildTask::class.java))
+//	}
 	compileOptions {
 		sourceCompatibility = JavaVersion.VERSION_1_8
 		targetCompatibility = JavaVersion.VERSION_1_8
@@ -64,12 +64,12 @@ dependencies {
 	implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.6.10"))
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
 }
-
-project.afterEvaluate{
-	for (buildTask in tasks.withType(CargoBuildTask::class.java)) {
-		tasks.withType(MergeSourceSetFolders::class.java).configureEach {
-			this.inputs.dir(layout.buildDirectory.dir("rustJniLibs" + File.separator + buildTask.toolchain!!.folder))
-			this.dependsOn(buildTask)
-		}
-	}
-}
+//
+//project.afterEvaluate{
+//	for (buildTask in tasks.withType(CargoBuildTask::class.java)) {
+//		tasks.withType(MergeSourceSetFolders::class.java).configureEach {
+//			this.inputs.dir(layout.buildDirectory.dir("rustJniLibs" + File.separator + buildTask.toolchain!!.folder))
+//			this.dependsOn(buildTask)
+//		}
+//	}
+//}
