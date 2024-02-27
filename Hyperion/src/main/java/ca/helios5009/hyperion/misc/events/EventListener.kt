40b292939e5dcf589b3fecbf9f4aad11d68b631e
Gilbert O. <giloe.dev@gmail.com>
2024-02-26 17:16:22 -0700

    Made sure to add coroutine closure after Event is finished

--- Hyperion/src/main/java/ca/helios5009/hyperion/misc/events/EventListener.kt
+++ Hyperion/src/main/java/ca/helios5009/hyperion/misc/events/EventListener.kt
@@ -2,8 +2,6 @@ package ca.helios5009.hyperion.misc.events
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicReference
 
